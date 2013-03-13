d29b95414537b219a5a8cec51da77e11d2f5ae5f
Jiehan Zheng <zheng@jiehan.org>
2013-03-13 10:36:19 +0800

    brand new rails 4.0 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeddieTaxi::Application.config.secret_key_base = '106455475bca5fc9f5d598b6125479d1b2ba7ed1592484317e24164ef305f9e2716a99d30f62a118176f1c2c36f73f81816bc83b644d413526a4b69da14f2f47'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PeddieTaxi::Application.config.session_store :encrypted_cookie_store, key: '_peddie_taxi_session'
